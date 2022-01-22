9e80b900a14a095804c5b56cdb7b340b85f18b67
Carmen España <cespana2@alu.ucam.edu>
2022-01-23 00:53:40 +0100

    Principal 1

---
+++ PracticaFinal/PrincipalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrincipalViewController.swift
+//  PracticaFinal
+//
+//  Created by mcremote06 on 22/01/2022.
+//  Copyright © 2022 mcremote06. All rights reserved.
+//
+
+import UIKit
+
+class PrincipalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
